eb56bf0d2b336285333f17170037dacfcce21655
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2025-02-13 11:09:41 +0530

    add packages

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL =
 // process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
-export const BASE_URL = '';
+export const BASE_URL = 'https://mern-ecommerce-server-2grz.onrender.com/';
 export const PRODUCTS_URL = 'https://mern-ecommerce-server-2grz.onrender.com/api/v1/products';
 export const UPLOAD_URL = 'https://mern-ecommerce-server-2grz.onrender.com/api/v1/upload';
 export const USERS_URL = 'https://mern-ecommerce-server-2grz.onrender.com/api/v1/users';
