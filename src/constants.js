9842c5d3b56dc9fa8f11d82660e501a1420c06ae
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2025-02-13 10:58:48 +0530

    add api

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 // export const BASE_URL =
 // process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
 export const BASE_URL = '';
-export const PRODUCTS_URL = 'https://e-com-tara-server.onrender.com/api/v1/products';
-export const UPLOAD_URL = 'https://e-com-tara-server.onrender.com/api/v1/upload';
-export const USERS_URL = 'https://e-com-tara-server.onrender.com/api/v1/users';
-export const ORDERS_URL = 'https://e-com-tara-server.onrender.com/api/v1/orders';
-export const RAZORPAY_URL = 'https://e-com-tara-server.onrender.com/api/v1/payment';
+export const PRODUCTS_URL = 'https://mern-ecommerce-server-2grz.onrender.com/api/v1/products';
+export const UPLOAD_URL = 'https://mern-ecommerce-server-2grz.onrender.com/api/v1/upload';
+export const USERS_URL = 'https://mern-ecommerce-server-2grz.onrender.com/api/v1/users';
+export const ORDERS_URL = 'https://mern-ecommerce-server-2grz.onrender.com/api/v1/orders';
+export const RAZORPAY_URL = 'https://mern-ecommerce-server-2grz.onrender.com/api/v1/payment';
